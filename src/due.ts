03b8e04ca9fdb3f840928476184a78ef47e0a3c1
Kae Sluder <kaesluder@fastmail.com>
2023-09-22 14:19:34 -0400

    add docstrings for functions

--- src/due.ts
+++ src/due.ts
@@ -3,6 +3,14 @@ import { ITask } from './db';
 const DAY_INTERVAL = 1000 * 60 * 60 * 24;
 const WEEK_INTERVAL = DAY_INTERVAL * 7;
 
+/**
+ * Return true if two dates are on the "same day". Probably converts
+ * to local time zone. Good enough if both dates are flagged in the same
+ * TZ.
+ * @param d1 Date 1
+ * @param d2 Date 2
+ * @returns boolean true/false.
+ */
 export const sameDay = function (d1: Date, d2: Date) {
   //console.log(d1);
   return (
@@ -12,6 +20,16 @@ export const sameDay = function (d1: Date, d2: Date) {
   );
 };
 
+/**
+ * Text formatting for due dates. Returns day-of-week
+ * for nearby dates.
+ *
+ * Time flies like an eagle, fruit flies like a banana.
+ *
+ * @param taskDate due date of task in unix milliseconds
+ * @param now (optional) optional "now" value used for testing
+ * @returns
+ */
 export const formatTaskDate = function (
   taskDate: number | undefined,
   now?: Date | undefined
